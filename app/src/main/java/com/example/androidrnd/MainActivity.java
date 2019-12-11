8567769a9ffb87447d999cbd425c5f1a7dc207d6
Sayed <msiam18@gmail.com>
2019-12-11 23:37:39 +0600

    Intent Add

--- app/src/main/java/com/example/androidrnd/MainActivity.java
+++ app/src/main/java/com/example/androidrnd/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.androidrnd;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button2);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button2){
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+        }
     }
 }
